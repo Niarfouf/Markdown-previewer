731f86df46721ade3806b2b1a4a59c77abe55b3d
Tomas Niarfeix <tniarfeix@gmail.com>
2023-07-07 17:43:21 +0200

    "let's go"

---
+++ src/Editor.js
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFreeCodeCamp } from "@fortawesome/free-brands-svg-icons";
+import { Container, Form } from "react-bootstrap/";
+import { useContext } from "react";
+import { MyContext } from "./MyContext";
+
+function Editor() {
+  const { text, setText } = useContext(MyContext);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <Container className="m-5 mb-0 w-25">
+      <Form>
+        <Form.Group>
+          <Form.Label className="fw-bold text-start bg-secondary w-100 m-0 p-2">
+            <FontAwesomeIcon icon={faFreeCodeCamp} /> Editor
+          </Form.Label>
+
+          <Form.Control
+            value={text}
+            id="editor"
+            onChange={handleTextChange}
+            as="textarea"
+            className="rounded-0"
+          />
+        </Form.Group>
+      </Form>
+    </Container>
+  );
+}
+
+export default Editor;
---
+++ src/Previewer.js
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFreeCodeCamp } from "@fortawesome/free-brands-svg-icons";
+import { Container, Form } from "react-bootstrap/";
+import { useContext } from "react";
+import { MyContext } from "./MyContext";
+import Markdown from "marked-react";
+
+function Previewer() {
+  const { text, setText } = useContext(MyContext);
+
+  return (
+    <Container className="m-5 w-50">
+      <Form>
+        <Form.Group>
+          <Form.Label className="fw-bold text-start text-black bg-white w-100 m-0 p-2">
+            <FontAwesomeIcon icon={faFreeCodeCamp} /> Previewer
+          </Form.Label>
+
+          <p id="preview" className="bg-secondary p-2">
+            <Markdown gfm="true" breaks="true">
+              {text}
+            </Markdown>
+          </p>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
+}
+
+export default Previewer;
